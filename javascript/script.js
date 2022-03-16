f64a8fd5db9de975cc9ad2fec45f4925b256ec59
Corey Lindsey <corey.lindsey.r@gmail.com>
2022-03-16 09:42:51 -0400

    misc updates.

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,38 @@
-let firstName = 'Mikey';
-let x = `Hey ${firstName}`;
+// let firstName = 'Mikey';
+// let x = `Hey ${firstName}`;
 
-console.log(x, firstName, x);
-console.log(typeof firstName);
+// console.log(x, firstName, x);
+// console.log(typeof firstName);
+
+const user = {
+    firstName: 'Corey',
+    lastName: 'Lindsey'
+};
+// const numb = [1, 2, 3];
+// let nims = [3, 1, 5];
+// function hey() {
+// console.log(user, firstName, user.firstName);
+// console.log(numb[3 -1]);
+// }
+
+// while (nims.length < 50) {
+//     hey();
+//     nims++;
+// }
+let slimShady = 0;
+
+function hiMyNameIs() {
+while (slimShady < 10) {
+console.log(user.firstName, user.lastName);
+slimShady++;
+}
+}
+hiMyNameIs();
+// hey();
+function yoNameIs() {
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+    hiMyNameIs(slimShady = 0);
+}
+}
+yoNameIs();
